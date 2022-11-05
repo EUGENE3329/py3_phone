e695a47b488f38491edfcc3dad2fb37914781964
EUGENE3329 <110486048+EUGENE3329@users.noreply.github.com>
2022-11-06 03:27:40 +0900

    makeing new empty file with txt obj

--- random_test.py
+++ random_test.py
@@ -1,10 +1,11 @@
 #!/urs/bin/python3
 import random
 
-#### from 1 to 46
+#### from 1 to 46, 난수 한 개만 생성
 i=random.randint(1,47)
 print( " picking ONE : ", i )
 
+#### 로또 흉내내기, imitation lottery
 numlist=range(1,47)
 s=random.sample(numlist,6)
 print("1st lottery, not ordered \n ",  s )
