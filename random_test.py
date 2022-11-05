aa8660eee3a3f1ae30e111194b3674fe1455718c
a503603@gmail.com <a503603@gmail.com>
2022-11-05 16:34:00 +0900

    ???

--- random_test.py
+++ random_test.py
@@ -1,13 +1,14 @@
 #!/urs/bin/python3
 import random
 
-#### from 1 to 45
-i=random.randint(1,46)
+#### from 1 to 46
+i=random.randint(1,47)
 print( " picking ONE : ", i )
 
-numlist=range(1,46)
+numlist=range(1,47)
 s=random.sample(numlist,6)
 print("1st lottery, not ordered \n ",  s )
 s.sort()
 print("1st lottery : \n ",  s )
 print(type(s))
+#
